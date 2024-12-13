532980107d54035a3a834a0ffe5e8792007fe2de
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2024-12-14 00:20:48 +0530

    day2.part2

---
+++ Day2/part2.py
@@ -0,0 +1,39 @@
+def is_safe_report(report):
+    
+    #for checking difference
+    for i in range(len(report) - 1):
+        diff = abs(report[i] - report[i+1])
+        if diff < 1 or diff > 3:
+            return False
+        
+    #for checking increasing or decreasing
+    increasing = decreasing = True
+    for i in range(len(report) - 1):
+        if report[i] < report[i + 1]:
+            decreasing = False
+        elif report[i] > report[i + 1]:
+            increasing = False
+
+    return increasing or decreasing
+
+
+def count_safe_reports(file_path):
+    safe_count = 0
+    with open(file_path, 'r') as file:
+        for line in file:
+            report = list(map(int, line.strip().split()))    
+            #without modification
+            if is_safe_report(report):
+                safe_count += 1
+            #after modification
+            else:
+                for i in range(len(report)):
+                    modified_report = report[:i] + report[i+1:]
+                    if is_safe_report(modified_report):
+                        safe_count += 1
+                        break
+    return safe_count
+
+file_path = "C:\\Users\\Akshaj Bansal\\OneDrive\\Desktop\\VS_CODE\\AdventOfCode_24\\Day2\\input.txt"
+result = count_safe_reports(file_path)
+print(result)
