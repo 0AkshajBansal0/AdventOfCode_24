242da1f64909eec50ae9d62fb871349614aff8ba
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2024-12-14 15:03:25 +0530

    day3.part1

---
+++ Day3/part1.py
@@ -0,0 +1,20 @@
+import re
+
+def process_memory_from_file(file_path):
+    total = 0
+    
+    with open(file_path, 'r') as file:
+        corrupted_memory = file.read()
+    
+    #Regular expression to match correct mul(X,Y)
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    matches = re.findall(pattern, corrupted_memory)
+    
+    for x, y in matches:
+        total += int(x) * int(y)
+    
+    return total
+
+file_path = "C:\\Users\\Akshaj Bansal\\OneDrive\\Desktop\\VS_CODE\\AdventOfCode_24\\Day3\\input.txt"
+result = process_memory_from_file(file_path)
+print("Total Sum:", result)
