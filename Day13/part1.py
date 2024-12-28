8450e67fe7526b83692893b61f5b7eef09f15968
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2024-12-29 00:20:49 +0530

    day13.part1

---
+++ Day13/part1.py
@@ -0,0 +1,36 @@
+from math import lcm
+from re import findall
+
+def q1(lines):
+    lines = " ".join(lines)
+    a = [(int(x), int(y)) for x, y in findall(r"Button A: X\+(\d+), Y\+(\d+)", lines)]
+    b = [(int(x), int(y)) for x, y in findall(r"Button B: X\+(\d+), Y\+(\d+)", lines)]
+    p = [(int(x), int(y)) for x, y in findall(r"Prize: X=(\d+), Y=(\d+)", lines)]
+
+    tokens = 0
+    for (ax, ay), (bx, by), (px, py) in zip(a, b, p):
+        m = lcm(ax, ay)
+
+        cx = m // ax
+        ax *= cx
+        bx *= cx
+        px *= cx
+
+        cy = m // ay
+        ay *= cy
+        by *= cy
+        py *= cy
+
+        assert ax == ay
+
+        b = (px - py) // (bx - by)
+        a = (px - bx * b) // ax
+
+        if a * ax + b * bx == px and a * ay + b * by == py:
+            tokens += 3 * a + b
+    return tokens
+
+if __name__ == "__main__":
+    with open("C:\\Users\\Akshaj Bansal\\OneDrive\\Desktop\\VS_CODE\\AdventOfCode_24\\Day13\\input.txt", "r") as file:
+        lines = file.readlines()
+    print(q1(lines))
